5797d05cf00ec6000db31547aa11ddceccfb76a0
awful_falafel <kybush1@gmail.com>
2016-01-05 23:40:32 -0600

    Initial Commit

---
+++ python/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Kyle Bush',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'kybush1@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
